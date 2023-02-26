3d1a99a1f5edb01d29eb9c6c1bd5018e06de0768
sendoru <sendol39@gmail.com>
2023-02-27 00:51:03 +0900

    Add spline interpolation

--- midiapipe_skeleton.py
+++ midiapipe_skeleton.py
@@ -3,7 +3,10 @@ import cv2
 import mediapipe as mp
 import open3d as o3d 
 import numpy as np
+import scipy as sp
 import copy
+from collections import deque
+
 from constants import *
 from recon_3d_hand import recon_3d_hand
 
@@ -14,6 +17,24 @@ cap_l = cv2.VideoCapture("./output_L_near.mp4")
 cap_r = cv2.VideoCapture("./output_R_near.mp4")
 image_width = 640
 image_height = 480
+
+class LineWithTiming:
+    def __init__(self, t_prev: int, p_prev: np.ndarray, t_cur: int, p_cur: np.ndarray):
+        self.t_prev = t_prev
+        self.p_prev = p_prev
+        self.t_cur = t_cur
+        self.p_cur = p_cur
+
+        self.pointcloud = o3d.geometry.PointCloud(o3d.utility.Vector3dVector(np.array([p_prev, p_cur])))
+        self.lines = o3d.geometry.LineSet()
+        self.lines.points = self.pointcloud.points
+        self.lines.lines = o3d.utility.Vector2iVector([[0, 1]])
+
+    def get_p_prev(self):
+        return self.pointcloud.points[0]
+
+    def get_p_cur(self):
+        return self.pointcloud.points[-1]
 
 def preprocess(image: np.ndarray, intrinsic_mat: np.ndarray, distortion_coeff: np.ndarray, hands):
     image = cv2.undistort(image, L_INTRINSIC, L_DISTORTION)
@@ -28,8 +49,6 @@ def preprocess(image: np.ndarray, intrinsic_mat: np.ndarray, distortion_coeff: n
 def draw_hand(results, image):
     for hand_no, hand_landmarks in enumerate(results.multi_hand_landmarks):
         print(f'HAND NUMBER: {hand_no+1}')
-        print('-----------------------')
-        # print(hand_landmarks)
         
         mp_drawing.draw_landmarks(
             image,
@@ -38,6 +57,8 @@ def draw_hand(results, image):
             mp_drawing_styles.get_default_hand_landmarks_style(),
             mp_drawing_styles.get_default_hand_connections_style())
 
+
+# initialize
 vis = o3d.visualization.Visualizer()
 vis.create_window(width=1600, height=900)
 threshold = 0.01
@@ -56,7 +77,13 @@ hands_r = mp_hands.Hands(
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5)
 
+recent_points = deque()
+recent_timings = deque()
 prev_point = None
+frame_no = 0
+
+
+# loop
 while cap_l.isOpened():
     time_elapsed = time.time()
     success_l, image_l = cap_l.read()
@@ -76,10 +103,6 @@ while cap_l.isOpened():
 
     hand_3d = recon_3d_hand(image_l, image_r, results_l, results_r)
 
-    # 어쩌면 제일 큰 문제
-    # 노트북에서 3프레임 나옴
-    # ㅅㅂ?
-
     # 해볼만한거
     # 1. 범위에 제약조건을 걸자
     # 현실적으로 범위가 4m? 정도 이상 나올일이 없을 것 같으니까 그냥 그 밖으로 튀어나가면 점을 렌더링하지 않음
@@ -90,19 +113,47 @@ while cap_l.isOpened():
     # 화면 밖으로 너무 튀어나가면 트래킹을 멈추게 하자
     if type(hand_3d) != type(None):
         # 8번점 = 검지끝
-        if np.linalg.norm(hand_3d[0].points[8]) <= 4000:
-            if prev_point is not None:
-                cur_points = o3d.geometry.PointCloud(o3d.utility.Vector3dVector(np.array([prev_point, hand_3d[0].points[8]])))
-                vis.add_geometry(cur_points)
+        # TODO 이거 바꿔야됨
+        # 점 하나의 좌표값 기반 -> 점 두개의 거리 기반(이동속도)
+
+        if len(recent_points) == 0:
+            recent_points.append(hand_3d[0].points[8])
+            recent_timings.append(frame_no / FRAME_RATE)
+
+        else:
+            speed = np.linalg.norm(hand_3d[0].points[8] - recent_points[-1]) / ((frame_no / FRAME_RATE) - recent_timings[-1])
+            if np.linalg.norm(hand_3d[0].points[8]) <= DIST_THRESHOLD and speed <= SPEED_THRESHOLD and np.abs(hand_3d[0].points[8][2]) >= DEPTH_MIN_THRESHOLD:
+                cur_points = o3d.geometry.PointCloud(o3d.utility.Vector3dVector(np.array([hand_3d[0].points[8], recent_points[-1]])))
+                # vis.add_geometry(cur_points)
                 line = o3d.geometry.LineSet()
                 line.points = cur_points.points
                 line.lines = o3d.utility.Vector2iVector([[0, 1]])
-                vis.add_geometry(line)
-            prev_point = hand_3d[0].points[8]
-            # vis.add_geometry(hand_3d[0])
-            # vis.add_geometry(hand_3d[1])
-            vis.poll_events()
-            vis.update_renderer()
+                # vis.add_geometry(line)
+                recent_points.append(hand_3d[0].points[8])
+                recent_timings.append(frame_no / FRAME_RATE)
+
+        while len(recent_points) > 5:
+            recent_points.popleft()
+            recent_timings.popleft()
+
+        # spline interpolation을 위해서는 기본 4개의 점이 필요 + 1개는 안정성을 위해 추가
+        if len(recent_points) == 5:
+            t = np.array(recent_timings)
+            dt = t[1:] - t[:-1]
+
+            # 손가락이 화면에서 너무 오랬동안 없어져 있던 경우에는 보간 중지
+            if max(dt) <= .5:
+                xy = np.array(recent_points)
+                tt = np.linspace(recent_timings[-2], recent_timings[-1], 30)
+                bspl = sp.interpolate.make_interp_spline(t, xy)
+                points = o3d.utility.Vector3dVector(bspl(tt))
+                pointcloud = o3d.geometry.PointCloud(points)
+                vis.add_geometry(pointcloud)
+        
+        # vis.add_geometry(hand_3d[0])
+        # vis.add_geometry(hand_3d[1])
+        vis.poll_events()
+        vis.update_renderer()
 
         # vis.remove_geometry(hand_3d[0])
         # vis.remove_geometry(hand_3d[1])
@@ -111,15 +162,16 @@ while cap_l.isOpened():
     image_full = np.concatenate((image_l, image_r), axis=1)
     cv2.imshow('MediaPipe Hands',image_full)
 
-    key = 0
     key = cv2.waitKey(1)
     if key & 0xFF == 27:
         break
     elif key & 0xFF == ord('p'):
         cv2.waitKey(0)
 
+    frame_no += 1
 
     print(f"FPS: {1 / (time.time() - time_elapsed):.3f}")
+    print('-'*20)
 cap_l.release()
 cap_r.release()
 cv2.destroyAllWindows()
